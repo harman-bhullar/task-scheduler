a2061cd2ecef8177660b0eb80fcbcbcaf05d3639
Harman Kaur <harmankaur022001@gmail.com>
2024-12-18 15:50:59 -0600

    Created two input div - 1. is for task 2. is for date

--- src/Task.js
+++ src/Task.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 export default function Task() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <h2 className="mb-4">Task Scheduler</h2>
+            <div className="row mb-4">
+                <div className="col-md-5">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter Task"
+
+                    />
+                </div>
+                <div className="col-md-5">
+                    <input
+                        type="date"
+                        className="form-control"
+
+                    />
+                </div>
+                
+            </div>
+        </div>
+    )
 }
